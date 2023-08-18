6281efaf5710f3e97260046c109981842aa530ed
Filip Parag <filip@parag.rs>
2023-08-18 09:39:42 +0200

    complete rewrite

---
+++ libwmrc.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+
+LOG_LEVEL="${LOG_LEVEL:-warn}"
+WMRC_CONFIG="$HOME/.config/wmrc"
+
+export _module
+export LOG_LEVEL
+export WMRC_CONFIG
+
+error() {
+    echo "$LOG_LEVEL" | grep -qi 'error\|warn\|info\|debug' || return 0
+    _title="$1"
+    shift 1
+    _message="$*"
+    >&2 printf '\033[1;37m[%s] \033[1;31m%s\033[0m%s %s\n' "$_module" "$_title" "${_message:+:}" "$_message"
+}
+
+warn() {
+    echo "$LOG_LEVEL" | grep -qi 'warn\|info\|debug' || return 0
+    _title="$1"
+    shift 1
+    _message="$*"
+    >&2 printf '\033[1;37m[%s] \033[1;33m%s\033[0m%s %s\n' "$_module" "$_title" "${_message:+:}" "$_message"
+}
+
+info() {
+   echo "$LOG_LEVEL" | grep -qi 'info\|debug' || return 0
+   _title="$1"
+    shift 1
+    _message="$*"
+    >&2 printf '\033[1;37m[%s] \033[1;32m%s\033[0m%s %s\n' "$_module" "$_title" "${_message:+:}" "$_message"
+}
+
+debug() {
+    echo "$LOG_LEVEL" | grep -qi 'debug' || return 0
+    _title="$1"
+    shift 1
+    _message="$*"
+    >&2 printf '\033[1;37m[%s] \033[1;34m%s\033[0m%s %s\n' "$_module" "$_title" "${_message:+:}" "$_message"
+}
+
+call() {
+    if [ -z "$1" ]; then
+        error 'Module name not provided'
+        return 1
+    else
+        _callee="$1"
+        shift 1
+    fi
+    debug 'Test module file' "$WMRC_CONFIG/modules/$_callee"
+    if ! test -f "$WMRC_CONFIG/modules/$_callee"; then
+        error 'Module not found' "$_callee"
+        exit 1
+    fi
+    sh -c "_module='$_callee' && . '$WMRC_DIR/libwmrc.sh' && . '$WMRC_CONFIG/modules/$_callee' && $*"
+    _status="$?"
+    if [ "$_status" != 0 ]; then
+        error 'Error executing command'
+    else
+        debug 'Command executed successfully'
+    fi
+}
+
+init() {
+    warn "Initialization method not defined for $_module"
+}
---
+++ wmrc.sh
@@ -0,0 +1,182 @@
+#!/bin/sh
+
+WMRC_DIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
+export WMRC_DIR
+
+_module='wmrc'
+. "$WMRC_DIR/libwmrc.sh"
+
+module_exec() {
+    check_dependencies "$1"
+    eval "call $*"
+}
+
+module_list() {
+    debug 'List modules'
+    debug 'Test module directory' "$WMRC_CONFIG/modules"
+    if ! test -d "$WMRC_CONFIG/modules"; then
+        error "Modules directory not found: $WMRC_CONFIG/modules"
+        exit 1
+    fi
+    modules="$(find "$WMRC_CONFIG/modules" -type f -executable -printf '%P\n' | grep 'test/')"
+    debug 'Found modules' "$(echo "$modules" | sed -z 's/\n/, /g;s/, $/\n/')"
+}
+
+get_dependencies() {
+    if [ -z "$1" ]; then
+        debug 'Get all dependencies'
+        module_list
+    else
+        debug 'Get dependencies for' "$1"
+        modules="$1"
+    fi
+    dependencies=""
+    for m in $modules; do
+        dependencies="$dependencies${dependencies:+:}$(call "$m" 'echo $WMRC_DEPENDENCIES' | sed 's/ \{1,\}/:/g')"
+    done
+    debug 'Found dependencies' "$(echo "$dependencies" | sed 's|:|, |g')"
+    dependencies="$(echo "$dependencies" | sed 's|:|\n|g' | sort | uniq)"
+}
+
+check_dependencies() {
+    debug 'Check dependencies'
+    get_dependencies "$1" || return 1
+    _missing=""
+    for d in $dependencies; do
+        if ! command -v "$d" 1>/dev/null; then
+            _missing="$_missing${_missing:+, }$d"
+        fi
+    done
+    if [ -n "$_missing" ]; then
+        error 'Missing dependencies' "$_missing"
+        exit 1
+    fi
+}
+
+read_config_variables() {
+    debug 'Read configuration variables'
+    debug 'Test configuration file' "$WMRC_CONFIG/rc.conf"
+    if ! test -f "$WMRC_CONFIG/rc.conf"; then
+        error 'Configuration file not found' "$WMRC_CONFIG/rc.conf"
+        exit 1
+    fi
+    _vars="$(
+        awk 'match($0, /^%(\w+) *= *(.*)$/, line) {
+            printf("export WMRC_%s=\"%s\"\n",line[1],line[2]);
+        }' "$WMRC_CONFIG/rc.conf"
+    )"
+    debug 'Load variables'
+    eval "$_vars"
+
+}
+
+config_unit_list() {
+    debug 'Test configuration file' "$WMRC_CONFIG/rc.conf"
+    if ! test -f "$WMRC_CONFIG/rc.conf"; then
+        error 'Configuration file not found' "$WMRC_CONFIG/rc.conf"
+        exit 1
+    fi
+    units="$(awk 'match($0, /^\[(\w+)\]$/, line) {
+        print line[1];
+    }' "$WMRC_CONFIG/rc_1.conf")"
+}
+
+run_config_unit() {
+    debug 'Test configuration file' "$WMRC_CONFIG/rc.conf"
+    if ! test -f "$WMRC_CONFIG/rc.conf"; then
+        error 'Configuration file not found' "$WMRC_CONFIG/rc.conf"
+        exit 1
+    fi
+    if [ -z "$1" ]; then
+        error 'Unit name not provided'
+        exit 1
+    fi
+    if ! grep -q "\[$1\]" "$WMRC_CONFIG/rc_1.conf"; then
+        error 'Configuration unit not found' "$1"
+        exit 1
+    fi
+    _unit="$(
+        awk -v target="$1" \
+        'match($0, /^\[(\w+)\]$/, line) {
+            section=line[1];
+        }
+        match($0, /^%.*$/) {
+            section="";
+        }
+        match($0, /^(\w+\/\w+)(::)?(\w+)?(\((.+)\))?/, line) {
+            if (section == target) {
+                module=line[1]
+                method=line[3] ? line[3] : "init"
+                args=line[5]
+                printf("module_exec \"%s\" \"%s\" %s\n", module, method, args);
+            }
+        }
+    ' "$WMRC_CONFIG/rc_1.conf")"
+    debug 'Execute unit'
+    eval "$_unit"
+}
+
+run_method() {
+    if [ -z "$1" ]; then
+        error 'Module name not provided'
+        exit 1
+    fi
+    _callee="$1"
+    if [ -z "$2" ]; then
+        _method="init"
+        shift 1
+    else
+        _method="$2"
+        shift 2
+    fi
+    _args="$*"
+    info 'Executing method' "$_callee::$_method($_args)"
+    eval "module_exec $_callee $_method $_args"
+}
+
+case "$1" in
+    "")
+        error 'No command specified'
+        ;;
+    "-v"|"--version"|"version")
+        echo 'wmrc 2.0.0'
+        ;;
+    "-h"|"--help"|"help")
+        printf 'wmrc 2.0.0\nFilip Parag <filip@parag.rs>\n\nCommands:\n'
+        printf '\tcall <module> <method> [args...]\n'
+        printf '\tunit <unit>\n'
+        printf '\tunits\n'
+        printf '\tmodules\n'
+        printf '\tdeps\n'
+        printf '\tcheck-deps\n'
+        printf '\thelp\n'
+        printf '\tversion\n'
+        ;;
+    "call")
+        shift 1
+        eval "run_method $*"
+        ;;
+    "unit")
+        shift 1
+        eval "run_config_unit $1"
+        ;;
+    "units")
+        config_unit_list
+        echo "$units"
+        ;;
+    "modules")
+        module_list
+        echo "$modules"
+        ;;
+    "deps")
+        get_dependencies
+        echo "$dependencies"
+        ;;
+    "check-deps")
+        check_dependencies
+        ;;
+    *)
+        error 'Unknown command'
+        ;;
+esac
+
