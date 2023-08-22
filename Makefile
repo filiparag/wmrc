5fc4690608d67eea9edbcd0e493578b889532d75
Filip Parag <filip@parag.rs>
2023-08-22 10:04:10 +0200

    fish completions

--- Makefile
+++ Makefile
@@ -4,10 +4,13 @@ install:
 	install -Dm 775 ./wmrc.sh /usr/bin/wmrc
 	sed -i 's/^WMRC_DIR=.*$$/WMRC_DIR=\/usr\/share\/wmrc/' /usr/bin/wmrc
 	install -Dm 664 ./libwmrc.sh /usr/share/wmrc/libwmrc.sh
+	install -dm 755 /usr/share/fish/completions
+	install -Dm 644 ./completions.fish /usr/share/fish/completions/wmrc.fish
 	install -dm 755 /usr/share/man/man1
 	gzip -ck wmrc.1.man > /usr/share/man/man1/wmrc.1.gz
 
 uninstall:
 	rm -f /usr/bin/wmrc
 	rm -fr /usr/share/wmrc
+	rm -f /usr/share/fish/completions/wmrc.fish
 	rm -f /usr/share/man/man1/wmrc.1.gz
---
+++ completions.fish
@@ -0,0 +1,49 @@
+#!/usr/bin/env fish
+
+function __fish_wmrc_root_command
+    set cmd (commandline -opc)
+    if [ (count $cmd) -eq 1 -a $cmd[1] = wmrc ]
+        return 0
+    end
+    return 1
+end
+
+function __fish_wmrc_using_command
+    set cmd (commandline -opc)
+    if [ (count $cmd) -eq 2 ]
+        if [ $argv[1] = $cmd[2] ]
+            return 0
+        end
+    end
+    return 1
+end
+
+function __fish_complete_wmrc
+    set __units (WMRC_LOG_LEVEL=none wmrc units)
+    set __modules (WMRC_LOG_LEVEL=none wmrc modules)
+    set __running (WMRC_LOG_LEVEL=none wmrc ps | awk 'NR>1 {print $2}')
+    complete -c wmrc -f
+    complete -c wmrc -n __fish_wmrc_root_command -a version -x -d 'Print version'
+    complete -c wmrc -n __fish_wmrc_root_command -a help -x -d 'Print help'
+    complete -c wmrc -n __fish_wmrc_root_command -a deps -x -d 'List of external dependencies required by all modules'
+    complete -c wmrc -n __fish_wmrc_root_command -a check-deps -x -d 'List of missing modules and external dependencies'
+    complete -c wmrc -n __fish_wmrc_root_command -a units -x -d 'List all units'
+    complete -c wmrc -n __fish_wmrc_root_command -a modules -x -d 'List all modules'
+    complete -c wmrc -n __fish_wmrc_root_command -a ps -x -d 'List all daemons running in the background'
+    complete -c wmrc -n __fish_wmrc_root_command -a var -x -d 'Print value of variable'
+    complete -c wmrc -n __fish_wmrc_root_command -a call -x -d 'Call module\'s method with arguments'
+    complete -c wmrc -n __fish_wmrc_root_command -a unit -x -d 'Runs all method calls in the unit'
+    complete -c wmrc -n __fish_wmrc_root_command -a start -x -d 'Start module\'s background daemon'
+    complete -c wmrc -n __fish_wmrc_root_command -a stop -x -d 'Stop module\'s background daemon'
+    complete -c wmrc -n __fish_wmrc_root_command -a restart -x -d 'Restart module\'s background daemon'
+    complete -c wmrc -n __fish_wmrc_root_command -a status -x -d 'Get module\'s background daemon status'
+    for cmd in call start
+        complete -c wmrc -f -n "__fish_wmrc_using_command $cmd" -a "$__modules"
+    end
+    for cmd in stop restart status
+        complete -c wmrc -f -n "__fish_wmrc_using_command $cmd" -a "$__running"
+    end
+    complete -c wmrc -f -n '__fish_wmrc_using_command unit' -a "$__units"
+end
+
+__fish_complete_wmrc
